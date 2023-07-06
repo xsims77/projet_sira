60e1fca1b8fbfc9f677a801ab9f37fe9a5730d2e
harounaKane <harouna.kane@hotmail.fr>
2023-07-06 10:42:54 +0200

    barre de navigation

--- model/ModelGenerique.php
+++ model/ModelGenerique.php
@@ -6,7 +6,7 @@ abstract class ModelGenerique
 
     public function __construct()
     {
-        $this->pdo = new PDO("mysql:host=127.0.0.1;dbname=projet_sira", "root", "", [
+        $this->pdo = new PDO("mysql:host=127.0.0.1;dbname=rh_projet_sira", "root", "", [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
        ]);
